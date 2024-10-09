ec3473e7e74d5800445fc29f1f37215c39be48b9
velvet-jedi <pranjalruhela2811@gmail.com>
2024-10-09 23:23:49 +0530

    movieSlice getNowPlaying movie state

--- src/Components/Browse.jsx
+++ src/Components/Browse.jsx
@@ -1,15 +1,19 @@
 import React, { useEffect } from "react";
 import Header from "./Header";
 import { API_OPTIONS } from "../utils/constants";
-
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../utils/movieSlice";
 const Browse = () => {
+	const dispatch = useDispatch();
+
 	const getNowPlaying = async () => {
 		const data = await fetch(
 			"https://api.themoviedb.org/3/movie/now_playing?&page=1",
 			API_OPTIONS
 		);
-		const json = data.json();
-		console.log(json);
+		const json = await data.json();
+		console.log(json.results);
+		dispatch(addNowPlayingMovies(json.results));
 	};
 
 	useEffect(() => {
